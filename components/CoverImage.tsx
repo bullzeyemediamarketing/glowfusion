eb077813a40f761e34ab00628e4724294de7e054
bullzeyemediamarketing <semrush@thebullzeye.com>
2025-03-06 01:05:30 +0600

    Update CoverImage.tsx

--- components/CoverImage.tsx
+++ components/CoverImage.tsx
@@ -1,3 +1,50 @@
+// import cn from 'classnames'
+// import { urlForImage } from 'lib/sanity.image'
+// import Image from 'next/image'
+// import Link from 'next/link'
+
+// interface CoverImageProps {
+//   title: string
+//   slug?: string
+//   image: any
+//   priority?: boolean
+// }
+
+// export default function CoverImage(props: CoverImageProps) {
+//   const { title, slug, image: source, priority } = props
+//   const image = source?.asset?._ref ? (
+//     <div
+//       className={cn('shadow-small', {
+//         'transition-shadow duration-200 hover:shadow-medium': slug,
+//       })}
+//     >
+//       <Image
+//         className="h-auto w-full"
+//         width={2000}
+//         height={1000}
+//         alt=""
+//         src={urlForImage(source).height(1000).width(2000).url()}
+//         sizes="100vw"
+//         priority={priority}
+//       />
+//     </div>
+//   ) : (
+//     <div style={{ paddingTop: '50%', backgroundColor: '#ddd' }} />
+//   )
+
+//   return (
+//     <div className="sm:mx-0">
+//       {slug ? (
+//         <Link href={`/posts/${slug}`} aria-label={title}>
+//           {image}
+//         </Link>
+//       ) : (
+//         image
+//       )}
+//     </div>
+//   )
+// }
+
 import cn from 'classnames'
 import { urlForImage } from 'lib/sanity.image'
 import Image from 'next/image'
@@ -8,10 +55,12 @@ interface CoverImageProps {
   slug?: string
   image: any
   priority?: boolean
+  alt?: string // Add alt as an optional prop
 }
 
 export default function CoverImage(props: CoverImageProps) {
-  const { title, slug, image: source, priority } = props
+  const { title, slug, image: source, priority, alt } = props
+
   const image = source?.asset?._ref ? (
     <div
       className={cn('shadow-small', {
@@ -22,7 +71,7 @@ export default function CoverImage(props: CoverImageProps) {
         className="h-auto w-full"
         width={2000}
         height={1000}
-        alt=""
+        alt={alt || title} // Use alt text passed as a prop or default to the post title
         src={urlForImage(source).height(1000).width(2000).url()}
         sizes="100vw"
         priority={priority}
